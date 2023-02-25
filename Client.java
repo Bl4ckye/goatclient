5863fec061b8d3a398036a9570f8ceaeab083683
Bl4ckye <120279299+Bl4ckye@users.noreply.github.com>
2023-02-25 19:25:07 +0100

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,67 @@
+package de.bl4ckye.client;
+
+import de.bl4ckye.client.cosmetics.CosmeticsManager;
+import de.bl4ckye.client.gui.overlays.FPSOverlay;
+import de.bl4ckye.client.gui.overlays.OverlayManager;
+import de.bl4ckye.client.servers.FeaturedServerData;
+import de.bl4ckye.discord.RPC;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ServerData;
+
+public class Client {
+
+    public static boolean initialized = false;
+
+
+    public static Minecraft minecraft;
+    public static OverlayManager overlayManager;
+    public static CosmeticsManager cosmeticsManager;
+
+
+    public static String websiteUrl = "https://goatclient.de";
+    public static String Discord = "https://dsc.gg/goatclient";
+    //public static String commingSoon = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    //public static CompoundTag overlayConfig;
+    public static String currentVersion = "12w1a";
+    public static String versionStatus = "SNAPSHOT";
+
+
+
+
+
+    public static void init(Minecraft minecraft){
+       Client.minecraft = minecraft;
+       overlayManager = new OverlayManager().init();
+       overlayManager.showOverlay(FPSOverlay.class);
+       overlayManager.hideOverlay(FPSOverlay.class);
+       cosmeticsManager = new CosmeticsManager().init();
+       //minecraft.setScreen(new WelcomeScreen());
+       initialized = true;
+
+    }
+
+    public static boolean isFeaturedServer(ServerData data){
+        return (data instanceof FeaturedServerData);
+    }
+
+    public static void tick(){
+        if(minecraft.level != null){
+            cosmeticsManager.getCapeManager().tick();
+            overlayManager.showOverlay(FPSOverlay.class);
+        }
+        else {
+            overlayManager.hideOverlay(FPSOverlay.class);
+        }
+    }
+
+
+    public static void resetRPC(){
+        RPC.instance().update("Main Menu","","","");
+    }
+
+
+    public static boolean isInitialized(){
+        return initialized;
+    }
+
+}
